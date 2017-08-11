9701729cb2125df48c048096fe0e0b056de60344
davidtspf <david.tannenbaum@gmail.com>
2017-08-11 16:34:26 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def begins_with_r(array)
   array.all? do |array|
     array[0] == "r"
@@ -23,10 +24,72 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  array_count = Hash.new()
-  array.each do |each_element|
-    require 'pry'
-    binding.pry
-    array_count[each_element] += 1
+  array_count = []
+  array.each do |name_hash|
+
+    if array_count == []
+      name_hash[:count] = 1
+      array_count.push(name_hash)
+    else
+      array_count.each do |name_whatever|
+        if name_whatever[:name] == name_hash[:name]
+          name_whatever[:count] += 1
+        end
+      end
+    end
+
   end
+  array_count
+end
+
+def count_elements(array)
+  counts = []
+  array.each do |name_hash|
+    if hash = counts.detect{|hash| hash[:name] == name_hash[:name] }
+      hash[:count] += 1
+    else
+      name_hash[:count] = 1
+      counts.push(name_hash)
+    end
+  end
+  counts
+end
+
+# def merge_data(arr1,arr2)
+#       merged =[ ]
+#       merged << arr1[0].merge(arr2[0]["blake"])
+#       merged << arr1[1].merge(arr2[0]["ashley"])
+# end
+
+def merge_data(array1, array2)
+  merged = []
+  array1.each_index do |i|
+    array2[0].keys.each do |name|
+      merged << array1[i].merge(array2[0][name]) if name == array1[i][:first_name]
+    end
+  end
+  merged
+end
+
+# let(:cool) {
+#   [
+#           {
+#                  :name => "ashley",
+#           :temperature => "sort of cool"
+#       },
+#           {
+#                  :name => "blake",
+#           :temperature => "cool"
+#       }
+#   ]
+# }
+
+def find_cool(cool)
+    cool.select {|i| i.any? {|k,v| v == "cool"}}
+end
+
+def organize_schools(schools)
+    locations_hash = {}
+    schools.collect {|k,v| locations_hash[v[:location]] = []}
+    locations_hash.each {|k,v| schools.each {|k1,v1| if k == v1[:location] then v << k1  end}}
 end
